920d0412e723b3234f33a0aa4af9d1a5e52d09ea
ND <nityanshdoshi2001@gmail.com>
2021-05-11 22:36:23 +0530

    Analysis of Cases Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from './SARS-CoV-2.png';
+import './App.css'; 
+import {Navbar, Nav, Button, Form, FormControl} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar bg="primary" variant="dark" >
+    <Navbar.Brand href="#home"><img src={logo} style={{width:"4vw"}}></img> COVID-19 Analysis</Navbar.Brand>
+    <h2 className="text-white">IGT Project</h2>
+    <Nav className="ml-auto">
+      <Nav.Link href="/cases" className="text-white links">Analysis of Cases</Nav.Link>
+      <Nav.Link href="/vaccination" className="text-white links">Analysis of Vaccinations</Nav.Link>
+    </Nav>
+
+  </Navbar>
     </div>
+    <Switch>
+      <Route path="/cases"><iframe width="100%" frameborder="0" height="768px"src="https://www.arcgis.com/apps/dashboards/470dc9708d434f09909f412d4eb2f237"  className="frame"></iframe></Route>
+    </Switch>
+    </Router>
   );
 }
 
